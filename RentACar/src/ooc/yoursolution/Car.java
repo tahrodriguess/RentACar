971ac9f7e5e4bc1d1c4fd08e4fdf84e29e328b39
tahrodriguess <tahrodriguesd@hotmail.com>
2021-11-02 22:01:18 +0000

    Class Car

---
+++ RentACar/src/ooc/yoursolution/Car.java
@@ -0,0 +1,19 @@
+package ooc.yoursolution;
+
+import ooc.yoursolution.CarInterface;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TahR
+ */
+public abstract class Car implements CarInterface {
+    
+    
+    
+}
