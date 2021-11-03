9568651f75c3b87f37f0c44bd3101f19a62eeb02
layhenececilio <layhenececilio@gmail.com>
2021-11-03 21:51:52 +0000

    finally kind fixed it

--- RentACar/src/ooc/yoursolution/Car.java
+++ RentACar/src/ooc/yoursolution/Car.java
@@ -4,6 +4,7 @@ package ooc.yoursolution;
 import java.util.Map;
 import ooc.enums.Make;
 import ooc.enums.Month;
+import java.util.HashMap;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -103,114 +104,5 @@ public class Car implements CarInterface {
 
         return false;
     }
-        /*private int id;
-        private Make make;
-        private double rate;
-        private  Map<Month, boolean[]> map;
-        
-        
-        public Car (int id, Make make, double rate){
-            
-            this.id = id;
-            this.make = make;
-            this.rate = rate;
-            createAvailability();
-        }
-        
-    @Override
-    public Map<Month, boolean[]> createAvailability()  {
-             
-        
-        map.put(Month.JANUARY, new boolean [31]);
-        map.put(Month.FEBRUARY, new boolean [28]);
-        map.put(Month.MARCH, new boolean [31]);
-        map.put(Month.APRIL, new boolean [30]);
-        map.put(Month.MAY, new boolean [31]);
-        map.put(Month.JUNE, new boolean [30]);
-        map.put(Month.JULY, new boolean [31]);
-        map.put(Month.AUGUST, new boolean [31]);
-        map.put(Month.SEPTEMBER, new boolean [30]);
-        map.put(Month.OCTOBER, new boolean [31]);
-        map.put(Month.NOVEMBER, new boolean [30]);
-        map.put(Month.DECEMBER, new boolean [31]);
-        
-        return map; 
-    }
-
-    @Override
-    public Make getMake() {
-        
-        return make;
-    }
-
-    @Override
-    public void setMake(Make make) {
-        
-        this.make = make;
-         }
-
-    @Override
-    public double getRate() {
-        
-        return rate;
-        
-    }
-
-    @Override
-    public void setRate(double rate) {
-        
-        this.rate = rate;
-    }
-
-    @Override
-    public Map<Month, boolean[]> getAvailability() {        
-            
-        return map;
-           
-    }
-
-    @Override
-    public void setAvailability(Map<Month, boolean[]> availability) {
-        
-        this.map = map;
-        
-    }
-
-    @Override
-    public int getId() {
-        
-        return id;
-    
-    }
-
-    @Override
-    public boolean isAvailable(Month month, int day) {
-            
-            boolean[] availability = map.get(month);
-            Boolean convert = null;
-            
-                
-                if (availability[day-1] == convert) { //HEEEEEELP
-                    availability[day-1] = true;
-                }// I don't know what to do I'm thinking of another way to write it because boolean is a primitive type and can not read null.
-                
-        return availability[day-1];
-        
-        
-      }  
-    
-    @Override
-    public boolean book(Month month, int day) {
-        if (map.get(month)[day-1]){
-            map.get(month)[day-1] = false;
-            return true;
-            
-        }
-                        
-        return false;
-        
-    }
-      
-   */ 
     
 }
