e6940a52ab11639cdf02a1509ccde99cdb9d3770
tahrodriguess <tahrodriguesd@hotmail.com>
2021-11-03 15:24:32 +0000

    Car class override

--- RentACar/src/ooc/yoursolution/Car.java
+++ RentACar/src/ooc/yoursolution/Car.java
@@ -1,6 +1,8 @@
 package ooc.yoursolution;
 
-import ooc.yoursolution.CarInterface;
+import java.util.Map;
+import ooc.enums.Make;
+import ooc.enums.Month;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,7 +14,57 @@ import ooc.yoursolution.CarInterface;
  *
  * @author TahR
  */
-public abstract class Car implements CarInterface {
+public class Car implements CarInterface {
+
+    @Override
+    public Map<Month, boolean[]> createAvailability() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Make getMake() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setMake(Make make) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getRate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setRate(double rate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<Month, boolean[]> getAvailability() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setAvailability(Map<Month, boolean[]> availability) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isAvailable(Month month, int day) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean book(Month month, int day) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     
