d5c821e517a5ceb9225dfc5258d42f32214247bf
layhenececilio <layhenececilio@gmail.com>
2021-11-03 14:48:07 +0000

    Implementation to Booking System Interface

--- RentACar/src/ooc/yoursolution/BookingSystem.java
+++ RentACar/src/ooc/yoursolution/BookingSystem.java
@@ -5,13 +5,21 @@
  */
 package ooc.yoursolution;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  *
  * @author layhenececilio
  */
-public class BookingSystem {
+public class BookingSystem implements BookingSystemInterface{
 
     public BookingSystem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
