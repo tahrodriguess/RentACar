43de4d44fe36e46371a667c0aaaefd5d806aace5
tahrodriguess <tahrodriguesd@hotmail.com>
2021-11-03 19:06:12 +0000

    Update Car.java

--- RentACar/src/ooc/yoursolution/Car.java
+++ RentACar/src/ooc/yoursolution/Car.java
@@ -77,29 +77,45 @@ public class Car implements CarInterface {
     }
 
     @Override
-    public Map<Month, boolean[]> getAvailability() {
-        
+    public Map<Month, boolean[]> getAvailability() {        
+            
+        return map;
            
     }
 
     @Override
     public void setAvailability(Map<Month, boolean[]> availability) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        this.map = map;
+        
     }
 
     @Override
     public int getId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        return id;
+    
     }
 
     @Override
     public boolean isAvailable(Month month, int day) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+            
+            boolean[] available = map.get(month);
+                
+                if (available){
+                    available = true;
+                }
+        
+        return available[day-1];        
+       }             
+    
     @Override
     public boolean book(Month month, int day) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            
+
+        
+        return false;
+        
     }
     
     
