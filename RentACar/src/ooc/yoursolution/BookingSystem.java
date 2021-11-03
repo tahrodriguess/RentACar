2fdd4e09fafa0c460abce7d77487af318dd35b2e
layhenececilio <layhenececilio@gmail.com>
2021-11-03 23:00:18 +0000

    coments in the bookingSystem

--- RentACar/src/ooc/yoursolution/BookingSystem.java
+++ RentACar/src/ooc/yoursolution/BookingSystem.java
@@ -21,23 +21,23 @@ public class BookingSystem implements BookingSystemInterface{
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
         
         String su, name;
-        List<Car> cars = new ArrayList<>();
-        name = in.readLine();
+        List<Car> cars = new ArrayList<>(); // Array list for the Car class.
+        name = in.readLine();// gets the String name and gives it the function of reading the lines.
         
         while ((su = in.readLine()) !=null){ //read the next line until is null
-            String s[] = su.split(":");
+            String s[] = su.split(":"); // splits the ":" in the file.
             
-            Make make = Make.valueOf(s[0]);
-            double rate = Double.parseDouble(s[1]);
-            int carsNum = Integer.parseInt(s[2]);
+            Make make = Make.valueOf(s[0]); //
+            double rate = Double.parseDouble(s[1]); // creates a double for reading the values.
+            int carsNum = Integer.parseInt(s[2]); //creates a int.
             
-            for(int i=0; i<carsNum; i++){
+            for(int i=0; i<carsNum; i++){ //This loop initiates the couter in 0 if the couter is bigger than the lenght of carsNum it does the rest of the loop.
                 Car car = new Car(i, make, rate);
-                cars.add(car);
+                cars.add(car); // add a car.
             }
         }
         RentACarInterface rentACarInterface = new RentACar(cars, name);
-        return rentACarInterface;
+        return rentACarInterface; //Will return the methods from Almicar class (rentACarInterface.
     }
     
 }
