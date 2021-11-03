d4fd149e61586efc5a5830f7c31897e7a48312dc
tahrodriguess <tahrodriguesd@hotmail.com>
2021-11-03 18:39:18 +0000

    Update Car.java

--- RentACar/src/ooc/yoursolution/Car.java
+++ RentACar/src/ooc/yoursolution/Car.java
@@ -1,6 +1,6 @@
 package ooc.yoursolution;
 
-import java.util.HashMap;
+
 import java.util.Map;
 import ooc.enums.Make;
 import ooc.enums.Month;
@@ -28,7 +28,6 @@ public class Car implements CarInterface {
             this.id = id;
             this.make = make;
             this.rate = rate;
-            this.map = map;
             createAvailability();
         }
         
@@ -54,27 +53,33 @@ public class Car implements CarInterface {
 
     @Override
     public Make getMake() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        return make;
     }
 
     @Override
     public void setMake(Make make) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        
+        this.make = make;
+         }
 
     @Override
     public double getRate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        return rate;
+        
     }
 
     @Override
     public void setRate(double rate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        this.rate = rate;
     }
 
     @Override
     public Map<Month, boolean[]> getAvailability() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+           
     }
 
     @Override
